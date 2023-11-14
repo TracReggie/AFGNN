728a76fcc598165fb6822d5080189139168c873a
Qi Zhang <52573258+TracReggie@users.noreply.github.com>
2023-11-14 08:54:10 +0800

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -1,6 +1,20 @@
 import torch
 import torch.nn.functional as F
-from training_tools import Logger, data_loader
+from torch_geometric.loader import DataLoader
+from torch_geometric.utils import mask_to_index
+
+from training_tools import set_seed, Logger
+from prepare_data import return_dataset
+from graph_tools import get_fea_list
+from model import AFGNN
+
+
+
+def data_loader(mask, batch_size, shuffle):
+    idx = mask_to_index(mask)
+    loader = DataLoader(idx, batch_size=batch_size, shuffle=shuffle)
+
+    return loader
 
 
 def train(train_loader, fea_list, mean_list, true_y, model, optimizer, device):
@@ -13,9 +27,9 @@ def train(train_loader, fea_list, mean_list, true_y, model, optimizer, device):
 
         optimizer.zero_grad()
         out = model(train_list, mean_list)
-        out = torch.log_softmax(out, dim=-1)
+        out = torch.softmax(out, dim=-1)
 
-        loss = F.nll_loss(out, train_y)
+        loss = F.cross_entropy(out, train_y)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
@@ -55,7 +69,19 @@ def attention_scores(fea_list, mean_list, model, device):
     return scores
 
 
-def run_experiments(args, model, masks, fea_list, data_y, device, save_scores=False):
+def run_experiments(args):
+    set_seed()
+    data = return_dataset(f'{args.dataset_name}')
+    device = torch.device(f'cuda:{args.cuda}')
+
+    masks = [data.train_mask, data.val_mask, data.test_mask]
+    [print(torch.sum(mask)) for mask in masks]
+
+    fea_list = get_fea_list(data, args.kl, args.km, args.kh)
+
+    model = AFGNN(data.num_features, len(fea_list), args.num_heads, args.att_dropout,
+                  args.hid_dim, data.num_classes, args.num_layers, args.dropout, args.adaptive).to(device)
+
     logger = Logger(args.runs, args)
     for run in range(args.runs):
         print('============================================')
@@ -71,18 +97,18 @@ def run_experiments(args, model, masks, fea_list, data_y, device, save_scores=Fa
         mean_list = [fea.to(device) for fea in mean_list]
         
         for epoch in range(1, 1 + args.epochs):
-            loss = train(train_loader, fea_list, mean_list, data_y, model, optimizer1, device)
+            loss = train(train_loader, fea_list, mean_list, data.y, model, optimizer1, device)
             print(f'Run: {run + 1:02d}, Epoch: {epoch:02d}, Loss: {loss:.4f}')
 
             if epoch % args.eval_steps == 0:
-                train_acc = test(fea_list, mean_list, data_y, train_loader, model, device)
-                val_acc = test(fea_list, mean_list, data_y, val_loader, model, device)
-                test_acc = test(fea_list, mean_list, data_y, test_loader, model, device)
+                train_acc = test(fea_list, mean_list, data.y, train_loader, model, device)
+                val_acc = test(fea_list, mean_list, data.y, val_loader, model, device)
+                test_acc = test(fea_list, mean_list, data.y, test_loader, model, device)
                 print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, Test: {test_acc:.4f}')
                 result = train_acc, val_acc, test_acc
                 logger.add_result(run, result)
         
-        if save_scores:
+        if args.save_scores:
             torch.save(attention_scores(fea_list, mean_list, model, device), f'scores_{run+1}.pt')
 
         logger.print_statistics(run)
