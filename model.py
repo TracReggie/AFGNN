2253aed36a506fd876aba3a98bdd8cb412e078c3
Qi Zhang <52573258+TracReggie@users.noreply.github.com>
2023-11-17 16:35:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -37,19 +37,18 @@ class MLP(torch.nn.Module):
         return x
 
 
-class MH_ListAtt(torch.nn.Module):
-    def __init__(self, fea_dim, num_fea, num_heads, dropout, att_dropout, adaptive=True):
-        super(MH_ListAtt, self).__init__()
+class MH_ATT(torch.nn.Module):
+    def __init__(self, fea_dim, num_feas, num_heads, E_dropout, att_dropout, adaptive):
+        super(MH_ATT, self).__init__()
 
         self.fea_dim = fea_dim
+        self.num_feas = num_feas
         self.num_heads = num_heads
 
-        self.dropout = torch.nn.Dropout(dropout)
-        self.att_dropout = torch.nn.Dropout(att_dropout)
-
-        self.E = MLP(fea_dim * num_fea, fea_dim * num_heads, fea_dim * num_heads, 3, dropout, 'prelu')
+        self.E = MLP(fea_dim * num_feas, fea_dim * num_heads, fea_dim * num_heads, 3, E_dropout, 'prelu')
         self.s = torch.nn.Parameter(torch.FloatTensor(num_heads, 2 * fea_dim, 1))
-        self.out_layer = torch.nn.Linear(fea_dim * num_heads, fea_dim)
+        self.out_layer = Linear(num_heads * fea_dim, fea_dim)
+        self.att_dropout = torch.nn.Dropout(att_dropout)
 
         self.act = torch.nn.Sigmoid()
         self.adaptive = adaptive
@@ -57,62 +56,68 @@ class MH_ListAtt(torch.nn.Module):
     def reset_parameters(self):
         self.E.reset_parameters()
         self.out_layer.reset_parameters()
-        torch.nn.init.normal_(self.s, std=0.001)
+        torch.nn.init.normal_(self.s, std=0.01)
 
-    def att_scores(self, fea_list, mean_list):
-        att_list = fea_list if self.adaptive else mean_list
-        cat_vector = torch.cat(att_list, dim=1)
+    def att_scores(self, batch_fea, mean_fea):  # [b, num_fea, fea_dim]
+        batch_att = batch_fea if self.adaptive else mean_fea
+        cat_vector = batch_att.reshape(batch_att.size(0), -1)  # [b, num_fea * fea_dim]
 
-        E = self.E(cat_vector)
-        E = E.view(-1, self.fea_dim, self.num_heads)  # [num_row, fea_dim, num_heads]
-        E = torch.permute(E, [2, 0, 1]).contiguous()  # [num_heads, num_row, fea_dim]
+        E = self.E(cat_vector)  # [b, fea_dim * num_heads]
+        E = E.reshape(-1, self.num_heads, self.fea_dim)  # [b, num_heads, fea_dim]
+        E_repeat = E.unsqueeze(2).repeat(1, 1, self.num_feas, 1)  # [b, num_heads, num_fea, fea_dim]
 
-        att_list = [fea.unsqueeze(0).repeat(self.num_heads, 1, 1).contiguous() for fea in att_list]
-        # [num_heads, num_row, fea_dim]
+        batch_att = batch_att.unsqueeze(1).repeat(1, self.num_heads, 1, 1)  # [b, num_heads, num_fea, fea_dim]
 
-        att_scores = [self.act(torch.matmul(torch.cat((E, fea), dim=2), self.s)).view(self.num_heads, -1, 1)
-                      for fea in att_list]
+        cat_vector = torch.cat([E_repeat, batch_att], dim=3)  # [b, num_heads, num_fea, 2 * fea_dim]
 
-        att_scores = torch.cat(att_scores, dim=2)
-        att_scores = F.softmax(att_scores - 0.5, dim=2)  # [num_heads, num_row, num_fea]
+        s = self.s.unsqueeze(0).repeat(batch_att.size(0), 1, 1, 1)  # [b, num_heads, 2 * fea_dim, 1]
+        
+        att_scores = self.act(torch.matmul(cat_vector, s))  # [b, num_heads, num_fea, 1]
+        att_scores = F.softmax(att_scores - 0.5, dim=2)  # [b, num_heads, num_fea, 1]
 
         return att_scores
 
-    def forward(self, fea_list, mean_list):
-        att_scores = self.att_scores(fea_list, mean_list)
+    def forward(self, batch_fea, batch_mean):
+        att_scores = self.att_scores(batch_fea, batch_mean)  # [b, num_heads, num_fea, 1]
+        att_scores = self.att_dropout(att_scores)
 
-        fea_list = [self.dropout(fea) for fea in fea_list]
-        fea_list = [fea.unsqueeze(0).repeat(self.num_heads, 1, 1).contiguous() for fea in fea_list]
+        fea_repeat = batch_fea.unsqueeze(1).repeat(1, self.num_heads, 1, 1)  #[b, num_heads, num_fea, fea_dim]
 
-        att_out = self.att_dropout(att_scores[:, :, 0].view(self.num_heads, -1, 1)) * fea_list[0]
+        output = fea_repeat * att_scores
+        output = torch.sum(output, dim=2)  # [b, num_heads, fea_dim]
+        output = output.reshape(-1, self.fea_dim * self.num_heads)
+        output = self.out_layer(output)
 
-        for i in range(1, len(fea_list)):
-            att_out += self.att_dropout(att_scores[:, :, i].view(self.num_heads, -1, 1)) * fea_list[i]
-        # att_out: [num_heads, num_row, fea_dim]
-
-        att_out = torch.permute(att_out, [1, 0, 2])  # [num_row, num_heads, fea_dim]
-        att_out = torch.reshape(att_out, [-1, self.fea_dim * self.num_heads])
-        att_out = self.out_layer(att_out)
-
-        return att_out
+        return output
 
 
 class AFGNN(torch.nn.Module):
-    def __init__(self, fea_dim, num_fea, num_heads, att_dropout, hid_dim, out_dim, num_layers, dropout, adaptive=True):
+    def __init__(
+            self,
+            fea_dim,
+            num_feas,
+            num_heads,
+            att_dropout,
+            hid_dim,
+            out_dim,
+            pred_layers,
+            dropout,
+            adaptive
+    ):
         super(AFGNN, self).__init__()
 
-        self.att = MH_ListAtt(fea_dim, num_fea, num_heads, dropout, att_dropout, adaptive)
-        self.pred = MLP(fea_dim, hid_dim, out_dim, num_layers, dropout)
+        self.mh_att = MH_ATT(fea_dim, num_feas, num_heads, dropout, att_dropout, adaptive)
+        self.pred = MLP(fea_dim, hid_dim, out_dim, pred_layers, dropout, 'prelu')
 
     def reset_parameters(self):
-        self.att.reset_parameters()
+        self.mh_att.reset_parameters()
         self.pred.reset_parameters()
 
-    def attention_scores(self, fea_list, mean_list):
-        return self.att.att_scores(fea_list, mean_list)
+    def attention_scores(self, batch_fea, batch_mean):
+        return self.mh_att.att_scores(batch_fea, batch_mean).squeeze()
 
-    def forward(self, fea_list, mean_list):
-        att_out = self.att(fea_list, mean_list)
+    def forward(self, batch_fea, batch_mean):
+        att_out = self.mh_att(batch_fea, batch_mean)
         out = self.pred(att_out)
 
         return out
