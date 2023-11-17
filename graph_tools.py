5b9a5a08645a3e56a1c68c3602fc858afea65a2b
Qi Zhang <52573258+TracReggie@users.noreply.github.com>
2023-11-17 16:35:12 +0800

    Update graph_tools.py

--- graph_tools.py
+++ graph_tools.py
@@ -16,27 +16,24 @@ def GCN_adj(edge_index, self_loops=True):
     return adj_sym
 
 
-def get_fea_list(data, kl, km, kh, pl=1.5, al=1, pm=-1.2, am=1, ph=1.5, ah=0.5, res=True):
+def get_features(data, kl, km, kh, pl=1.5, al=1, pm=-1.2, am=1, ph=1.5, ah=0.5, res=True):
     adj_sym = GCN_adj(data.edge_index)
-
-    fea_list = [data.x] if res else []
-
+    features = [data.x] if res else []
     xl, xm, xh = data.x, data.x, data.x
-
-    fea_list_l, fea_list_m, fea_list_h = [], [], []
 
     for _ in range(kl):
         xl = pl * (al * torch.from_numpy(adj_sym @ xl) + (1 - al) * xl)
-        fea_list_l.append(xl)
+        features.append(xl)
 
     for _ in range(km):
         xm = pm * (torch.from_numpy(adj_sym @ (adj_sym @ xm)) - am * xm)
-        fea_list_m.append(xm)
+        features.append(xm)
 
     for _ in range(kh):
         xh = ph * ((-ah) * torch.from_numpy(adj_sym @ xh) + (1 - ah) * xh)
-        fea_list_h.append(xh)
+        features.append(xh)
 
-    fea_list = fea_list + fea_list_l + fea_list_m + fea_list_h
+    features = torch.cat(features, dim=1)
+    features = features.view(data.num_nodes, 1+kl+km+kh, -1)
 
-    return fea_list
+    return features
